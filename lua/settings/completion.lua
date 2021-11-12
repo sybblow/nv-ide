030b943975b34afeacb6478005704f1cb95d28d1
Siliang Cao <siliang.cao@gmail.com>
2021-11-13 00:01:43 +0800

    Configure completion

--- lua/settings/completion.lua
+++ lua/settings/completion.lua
@@ -21,9 +21,9 @@ cmp.setup({
   mapping = {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-/>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
   },
   sources = {
     { name = 'nvim_lsp' },
